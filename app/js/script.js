e148929238103848fa5ca2021bb112cacc4ac755
Vyacheslav <mister.slaus@gmail.com>
2018-05-02 17:43:14 +0300

    Add files via upload

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,19 @@
 //Scripts
+
+//Back to top button
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 400) {
+        $('#back-to-top').fadeIn();
+    } else {
+        $('#back-to-top').fadeOut();
+    }
+});
+// scroll body to 0px on click
+$('#back-to-top').click(function () {
+    $('#back-to-top').tooltip('hide');
+    $('body,html').animate({
+        scrollTop: 0
+    }, 800);
+    return false;
+});
+
